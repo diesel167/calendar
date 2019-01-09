6c7b510beea88ac972d200854dd20ca6966f3908
Yury Misiyevich <vinchesterok@tut.by>
2019-01-09 15:35:37 +0300

    build added

---
+++ build/precache-manifest.834171deedfb47ae808c31a1b1eb0c81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3affe1246aa3580436b0",
+    "url": "/static/js/main.3affe124.chunk.js"
+  },
+  {
+    "revision": "97b06386d8686af4b47e",
+    "url": "/static/js/1.97b06386.chunk.js"
+  },
+  {
+    "revision": "3affe1246aa3580436b0",
+    "url": "/static/css/main.2e215e1b.chunk.css"
+  },
+  {
+    "revision": "97b06386d8686af4b47e",
+    "url": "/static/css/1.60a7f080.chunk.css"
+  },
+  {
+    "revision": "88a6bac0bfb7d5d44e2432d2be45b0e2",
+    "url": "/index.html"
+  }
+];
