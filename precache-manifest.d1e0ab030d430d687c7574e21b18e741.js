5a4e51af9a60403e133714e973c7bc45a8b08b20
Yury Misiyevich <misiyevich@gmail.com>
2020-10-13 11:51:30 +0300

    feat: build

---
+++ precache-manifest.d1e0ab030d430d687c7574e21b18e741.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "80ab4dd4aba20a379df8",
+    "url": "/calendar/static/css/main.d554cd5e.chunk.css"
+  },
+  {
+    "revision": "80ab4dd4aba20a379df8",
+    "url": "/calendar/static/js/main.80ab4dd4.chunk.js"
+  },
+  {
+    "revision": "b46b60a8dfe93d29ea3a",
+    "url": "/calendar/static/css/1.1ad1940b.chunk.css"
+  },
+  {
+    "revision": "b46b60a8dfe93d29ea3a",
+    "url": "/calendar/static/js/1.b46b60a8.chunk.js"
+  },
+  {
+    "revision": "d8c9fae71e5d28e44ecc",
+    "url": "/calendar/static/js/runtime~main.d8c9fae7.js"
+  },
+  {
+    "revision": "ea6a748b94e110f22f65eed54b6369d7",
+    "url": "/calendar/index.html"
+  }
+];
